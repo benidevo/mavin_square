b526578dac840257e940ab0220cec10dd1028d14
BENJAMIN IDEWOR <benjamin.idewor@decagon.dev>
2021-07-03 21:53:22 +0100

    add carousel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
+import Featured from './components/featured/index'
+
 import './App.css';
 
 const App = () => {
     return (
         <div className="App">
             <Header />
+            <>
+                <Featured />
+            </>
             <Footer />
         </div>
     )
---
+++ src/components/featured/Carousel.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import slide_one from '../../resources/images/slide_one.jpg';
+import slide_two from '../../resources/images/slide_two.jpg';
+import slide_three from '../../resources/images/slide_three.jpg';
+
+
+
+const Carousel = () => {
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        autoplay: true,
+    }
+
+    return (
+        <div
+            className="carrousel_wrapper"
+            style={{
+                height: `${window.innerHeight}px`,
+            }}
+        >
+            <Slider {...settings}>
+                <div>
+                    <div
+                        className="carrousel_image"
+                        style={{
+                            background: `url(${slide_one})`,
+                            height: "100vh"
+                        }}
+                    >
+                    </div>
+                </div>
+                <div>
+                    <div
+                        className="carrousel_image"
+                        style={{
+                            background: `url(${slide_two})`,
+                            height: "100vh"
+                        }}
+                    >
+                    </div>
+                </div>
+                <div>
+                    <div
+                        className="carrousel_image"
+                        style={{
+                            background: `url(${slide_three})`,
+                            height: "100vh"
+                        }}
+                    >
+                    </div>
+                </div>
+            </Slider>
+            
+        </div>
+    )
+}
+
+export default Carousel
